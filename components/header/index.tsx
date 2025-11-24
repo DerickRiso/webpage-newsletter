b1a49c786275fa76bee263dbf88ab95b95c74816
Derick Darlan <derickdarlan304@gmail.com>
2025-11-24 15:41:36 -0300

    feat: interatividade no header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,9 +1,7 @@
 
 export function Header() {
-    // Mudar a cor do header ao rolar a tela
-
     return (
-        <header className='flex items-center justify-between w-screen fixed top-0 pt-8 pb-8 pl-10 pr-10 bg-transparent' >
+        <header id="header" className='flex items-center justify-between w-screen fixed top-0 pt-8 pb-8 pl-10 pr-10 bg-transparent ' >
         <h1 className='text-4xl'>
           Hello, World!
         </h1>
--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import { SectionTopics } from "../components/section-topics"
 import { Header } from "../components/header"
 
 function App() {
+  document.addEventListener('scroll', changeHeaderColor);
 
   return (
     <>
       <Header />
 
       <main className='w-screen'>
-        <section className='flex gap-4 flex-col justify-center items-center bg-[url("./assets/background.png")] bg-cover bg-no-repeat w-screen h-180 top-0'>
+        <section id="home" className='flex gap-4 flex-col justify-center items-center bg-[url("./assets/background.png")] bg-cover bg-no-repeat w-screen h-180 top-0'>
 
           <h1 className='text-4xl z-2'>
             Comece a receber os emails agora!
@@ -32,6 +33,19 @@ function App() {
       </footer>
     </>
   )
+}
+
+function changeHeaderColor() {
+  const home = document.getElementById('home') as HTMLElement;
+  const header = document.getElementById('header') as HTMLHeadingElement;
+
+  let position = home.getBoundingClientRect()
+  if (position.y < -600) {
+    header.style.backgroundColor = "#000000"
+  }
+  if (position.y > -600) {
+    header.style.backgroundColor = "transparent"
+  }  
 }
 
 export default App
